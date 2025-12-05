06b73a1d78dd8c692f1a265e65c4ecaa2cdccb28
KopeckyTom <kopecky4@spsejecna.cz>
2025-12-05 14:05:24 +0100

    added ram monitoring

--- src/SystemMonitor.java
+++ src/SystemMonitor.java
@@ -1,24 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import com.sun.management.OperatingSystemMXBean;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 
 public class SystemMonitor {
 
     private static JProgressBar cpuBar;
+    private static JProgressBar ramBar;
 
     private static volatile int cpuPercent = 0;
+    private static volatile int ramPercent = 0;
 
 
     public static void main(String[] args) {
 
         createGUI();
         Thread cpuThread = new Thread(SystemMonitor::monitorCPU);
+        Thread ramThread = new Thread(SystemMonitor::monitorRAM);
         cpuThread.start();
+        ramThread.start();
     }
 
     private static void createGUI() {
@@ -30,10 +31,14 @@ public class SystemMonitor {
         cpuBar.setStringPainted(true);
         cpuBar.setBorder(BorderFactory.createTitledBorder("CPU Usage"));
 
+        ramBar = new JProgressBar(0, 100);
+        ramBar.setStringPainted(true);
+        ramBar.setBorder(BorderFactory.createTitledBorder("RAM Usage"));
 
 
         frame.setLayout(new GridLayout(4, 1));
         frame.add(cpuBar);
+        frame.add(ramBar);
         frame.setVisible(true);
     }
 
@@ -46,6 +51,19 @@ public class SystemMonitor {
             sleep(1000);
         }
     }
+
+    private static void monitorRAM() {
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        while (true) {
+            long totalPhysical = osBean.getTotalPhysicalMemorySize();
+            long freePhysical = osBean.getFreePhysicalMemorySize();
+            ramPercent = (int)((double)(totalPhysical - freePhysical) / totalPhysical * 100);
+
+            SwingUtilities.invokeLater(() -> ramBar.setValue(ramPercent));
+            sleep(1000);
+        }
+    }
+
 
 
     private static void sleep(int millis) {
